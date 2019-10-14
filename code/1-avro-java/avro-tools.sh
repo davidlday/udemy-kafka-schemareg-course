443ae302082b84c78b4b66efb156a41d717d0a80
David L. Day <dday376@gmail.com>
2019-10-14 13:20:32 -0400

    use homebrew

--- code/1-avro-java/avro-tools.sh
+++ code/1-avro-java/avro-tools.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+# use HomeBrew instead
+
 # put this in any directory you like
-wget http://central.maven.org/maven2/org/apache/avro/avro-tools/1.8.2/avro-tools-1.8.2.jar
+# wget http://central.maven.org/maven2/org/apache/avro/avro-tools/1.8.2/avro-tools-1.8.2.jar
+brew install avro-tools
 
 # run this from our project folder. Make sure ~/avro-tools-1.8.2.jar is your actual avro tools location
-java -jar ~/avro-tools-1.8.2.jar tojson --pretty customer-generic.avro 
-java -jar ~/avro-tools-1.8.2.jar tojson --pretty customer-specific.avro 
-
 # getting the schema
-java -jar ~/avro-tools-1.8.2.jar getschema customer-specific.avro 
+# java -jar ~/avro-tools-1.8.2.jar tojson --pretty customer-generic.avro
+avro-tools tojson --pretty customer-generic.avro
+
+# java -jar ~/avro-tools-1.8.2.jar tojson --pretty customer-specific.avro
+avro-tools tojson --pretty customer-specific.avro
+
+# java -jar ~/avro-tools-1.8.2.jar getschema customer-specific.avro
+avro-tools getschema customer-specific.avro
